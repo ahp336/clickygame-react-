4b718a3f33f669e152740c3ecd9de5972152c600
ahp336 <aakashparikh8@gmail>
2019-11-07 15:43:15 -0500

    Updates

---
+++ precache-manifest.98f1cd42c3877cc006cbcb493d52a293.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241a0b54671dc687316c587fa658e32b",
+    "url": "/clickygame-react-/index.html"
+  },
+  {
+    "revision": "8a098debb1b43029bc75",
+    "url": "/clickygame-react-/static/css/main.6aace5ee.chunk.css"
+  },
+  {
+    "revision": "b260a27e941a076dba8a",
+    "url": "/clickygame-react-/static/js/2.20f0a998.chunk.js"
+  },
+  {
+    "revision": "8a098debb1b43029bc75",
+    "url": "/clickygame-react-/static/js/main.cb555cf5.chunk.js"
+  },
+  {
+    "revision": "4744c90f1df2c1b7f1c3",
+    "url": "/clickygame-react-/static/js/runtime-main.e921f85a.js"
+  }
+]);
